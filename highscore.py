b546befa6fa55caf39dfc2ae35e86dfe528d4c14
hggks <carl.dahlen0068@stud.hkr.se>
2023-02-27 15:07:24 +0100

    uppdatera highscoor
    börja på highscore klassen

--- highscore.py
+++ highscore.py
@@ -1,2 +1,19 @@
 class Highscore:
-    ...
+    ''''Construktor for keeping the score '''
+    def __init__(self, player_name):
+        self.player_name = player_name
+        self.bot_name = "Bot"
+        self.player_score = 0
+        self.bot_score = 0
+    ''''getters and setters for the player and the bot'''
+    def setPlayer_score(self, score):
+        self.player_score += score
+
+    def setBot_score(self, score):
+        self.bot_score += score
+
+    def getplayer_score(self):
+        return self.player_score
+
+    def getbot_score(self):
+        return self.bot_score
