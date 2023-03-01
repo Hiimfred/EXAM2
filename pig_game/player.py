2fcdeff0fceb9957c9bb55b3e3954dab7e5ddefc
Hiimfred <Fredrik.ottenfelt0006@stud.hkr.se>
2023-03-01 15:56:50 +0100

    New update on player class
    changed make_move to roll and added hold function

--- pig_game/player.py
+++ pig_game/player.py
@@ -1,21 +1,17 @@
-"""CONTAINS A PLAYER CLASS"""
-# Commented out stuff that caused errors, did not remove anything.
-# from colorama import FORE, BACK, STYLE
+"""Contains a player class."""
 from dice import Dice
 
 
 class Player():
     """
-    PLAYER CLASS
+    PLAYER CLASS.
 
     CONTAINS GETTERS AND SETTERS FOR NAME, SCORE AND COLOR
-    AND A CLASS MAKE_PLAY FOR THE PLAYER TO MAKE A MOVE IN THE GAME
+    AND A CLASS MAKE_PLAY FOR THE PLAYER TO MAKE A MOVE IN THE GAME.
     """
-    # Removed this from _init_ arg ", color: FORE", caused error.
+
     def __init__(self, name: str, color: str):
-        """
-        DEFAULT PLAYER BLUEPRINT
-        """
+        """DEFAULT PLAYER BLUEPRINT."""
 
         self._total_score = 0
         self._current_score = 0
@@ -23,47 +19,56 @@ class Player():
         self._color = color
 
     def get_name(self):
-        """
-        GET THE PLAYER NAME
-        """
+        """GET THE PLAYER NAME."""
         return self._name
 
     def set_name(self, new_name: str):
-        """
-        SET THE PLAYER NAME WHEN ENTERED
-        """
+        """SET THE PLAYER NAME WHEN ENTERED."""
         self._name = new_name
 
     def get_score(self):
-        """
-        GET THE PLAYER CURRENT SCORE
-        """
+        """GET THE PLAYER CURRENT SCORE."""
         return self._current_score
 
+    def set_score(self, score):
+        """SET THE PLAYER NEW CURRENT SCORE."""
+        self._current_score = score
+
     def get_total_score(self):
-        """
-        GET THE PLAYER TOTAL SCORE
-        """
+        """GET THE PLAYER TOTAL SCORE."""
         return self._total_score
 
+    def set_total_score(self, new_tscore):
+        """SET THE PLAYER NEW TOTAL SCORE."""
+        self._total_score = new_tscore
+
     def get_color(self):
-        """
-        GET THE PLAYER COLOR
-        """
+        """GET THE PLAYER COLOR."""
         return self._color
 
     def set_color(self, new_color):
-        """
-        SETS THE PLAYERS CHOSEN COLOR
-        """
+        """SETS THE PLAYERS CHOSEN COLOR."""
         self._color = new_color
 
-    def make_play(self, dice: Dice):
+    def roll(self, dice: Dice):
         """
-        PLAYER MAKE A MOVE TO ROLL DICE AND
-        RUTURN OBTAINED SCORE
+        PLAYER ROLL THE DICE
+
+        IF THE OUTCOME IS 1 THE SCORE IS SET TO ZERO
+        ELSE ADD THE OUTCOME TO CURRENT SCORE.
         """
-        print("Rolling the dice... ")
-        # result = Dice.roll_dice
-        # current_score += result
-        # return current_score
+        outcome = dice.roll_dice()
+        if outcome == 1:
+            self.set_score(0)
+        else:
+            self.set_score(self.get_score() + outcome)
+
+        return outcome
+
+    def hold(self):
+        """
+        PLAYER HOLD
+
+        PLAYER HOLD AND ADDS THE CURRENT SCORE TO THE TOTAL SCORE
+        """
+        self.set_total_score(self.get_score() + self.get_total_score())
