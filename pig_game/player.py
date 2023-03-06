a72f4b6f6c1886c948204a6d7c976c9d41a90bf7
Hiimfred <Fredrik.ottenfelt0006@stud.hkr.se>
2023-03-06 15:47:19 +0100

    Update on test_player class
    new test method + correction docstrings

--- pig_game/player.py
+++ pig_game/player.py
@@ -4,16 +4,17 @@ from .dice import Dice
 
 class Player():
     """
-    PLAYER CLASS.
+    Player class.
 
-    CONTAINS GETTERS AND SETTERS FOR NAME, SCORE AND COLOR
-    AND A CLASS MAKE_PLAY FOR THE PLAYER TO MAKE A MOVE IN THE GAME.
+    Contains getter and setters for name, score, color and wins.
+    Also contains methods for player to roll, hold and
+    alter highscore numbers.
     """
 
     color_list = ["red", "blue", "green", "magenta", "yellow", "cyan"]
 
     def __init__(self, name: str, color: str):
-        """DEFAULT PLAYER BLUEPRINT."""
+        """Default player blueprint."""
 
         self._total_score = 0
         self._current_score = 0
@@ -22,35 +23,35 @@ class Player():
         self._wins = 0
 
     def get_name(self):
-        """GET THE PLAYER NAME."""
+        """Get the player name."""
         return self._name
 
     def set_name(self, new_name: str):
-        """SET THE PLAYER NAME WHEN ENTERED."""
+        """Set the player name when entered."""
         self._name = new_name
 
     def get_score(self):
-        """GET THE PLAYER CURRENT SCORE."""
+        """Get the player current score."""
         return self._current_score
 
     def set_score(self, score):
-        """SET THE PLAYER NEW CURRENT SCORE."""
+        """Set the player new current score."""
         self._current_score = score
 
     def get_total_score(self):
-        """GET THE PLAYER TOTAL SCORE."""
+        """Get the player total score."""
         return self._total_score
 
     def set_total_score(self, new_tscore):
-        """SET THE PLAYER NEW TOTAL SCORE."""
+        """Set the player new total score."""
         self._total_score = new_tscore
 
     def get_color(self):
-        """GET THE PLAYER COLOR."""
+        """Get the player color."""
         return self._color
 
     def set_color(self, new_color):
-        """SETS THE PLAYERS CHOSEN COLOR."""
+        """Set the player chosen color."""
         new_color = str.lower(new_color)
         if (new_color in self.color_list):
             self._color = new_color
@@ -59,20 +60,23 @@ class Player():
                              "red, blue, green, magenta, yellow, cyan")
 
     def add_win(self):
+        """Adds one win for player."""
         self._wins += 1
 
     def get_nr_of_wins(self):
+        """Get number of wins for a player."""
         return self._wins
 
     def set_nr_of_wins(self, nr_wins: int):
+        """Set number of wins for a player."""
         self._wins = nr_wins
 
     def roll(self, dice: Dice):
         """
-        PLAYER ROLL THE DICE
+        Player roll the dice.
 
-        IF THE OUTCOME IS 1 THE SCORE IS SET TO ZERO
-        ELSE ADD THE OUTCOME TO CURRENT SCORE.
+        If the outcome is 1 the score is set to zero
+        else add the outcome to current score.
         """
         outcome = dice.roll_dice()
         if outcome == 1:
@@ -84,12 +88,13 @@ class Player():
 
     def hold(self):
         """
-        PLAYER HOLD
+        Player hold.
 
-        PLAYER HOLD AND ADDS THE CURRENT SCORE TO THE TOTAL SCORE
+        Player hold and adds the current score to the total score.
         """
         self.set_total_score(self.get_score() + self.get_total_score())
         self.set_score(0)
 
     def __str__(self):
+        """Return a formated string with name and number of wins."""
         return f"{self.get_name():^10} ---- {self.get_nr_of_wins():^10}"
