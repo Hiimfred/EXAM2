62fb1169dbae4febcc3bafb1ac7220a5a1216835
Lucas <lucas.lofqvist@gmail.com>
2023-03-07 22:31:11 +0100

    commit

--- unittest_directory/test_game.py
+++ unittest_directory/test_game.py
@@ -8,21 +8,12 @@ class TestGame(unittest.TestCase):
 
     def setUp(self):
         """Set up objects for testing."""
-        self.curr_player = player.Player("P1", "blue")
-        self.pend_player = player.Player("P2", "red")
-        self.bot = intelligence.Intelligence()
-        self.die = dice.Dice()
-        self.highscore = highscore.Highscore()
+        self.game = game.Game()
+
+    def test_game__init__(self):
+        ...
 
     def test_new_difficulty(self):
         """Test new_difficulty method."""
-        self.game.start_solo_game("Player") 
-
-    def test_set_number_of_players(self):
-        self.assertRaises(ValueError, self.game.set_number_of_players, 3)
-        self.assertRaises(ValueError, self.game.set_number_of_players, 0)
-        self.assertRaises(ValueError, self.game.set_number_of_players, -1)
-        self.game.set_number_of_players(1)
-        self.assertEqual(self.game.get_number_of_players(), 1)
-        self.game.set_number_of_players(2)
-        self.assertEqual(self.game.get_number_of_players(), 2)
+        self.game.start_solo_game("Player")
+        
