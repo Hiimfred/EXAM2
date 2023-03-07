c3d8abec8263d921ea5d566f87293912a90c1c28
Lucas <lucas.lofqvist@gmail.com>
2023-03-07 15:06:44 +0100

    Quick update

--- unittest_directory/test_game.py
+++ unittest_directory/test_game.py
@@ -1,11 +1,6 @@
 """Containst TestGame class."""
 import unittest
 from pig_game import game
-from pig_game import player
-from pig_game import intelligence
-from pig_game import dice
-from pig_game import highscore
-from pig_game import difficulty
 
 
 class TestGame(unittest.TestCase):
@@ -13,14 +8,11 @@ class TestGame(unittest.TestCase):
 
     def setUp(self):
         """Set up objects for testing."""
-        self.curr_player = player.Player("P1", "blue")
-        self.pend_player = player.Player("P2", "red")
-        self.bot = intelligence.Intelligence()
-        self.die = dice.Dice()
-        self.highscore = highscore.Highscore()
+        self.game = game.Game()
 
     def test_game__init__(self):
     
     def test_new_difficulty(self):
         """Test new_difficulty method."""
-        self.bot.set_difficulty(difficulty.Difficulty.HARD)
+        ...
+        
