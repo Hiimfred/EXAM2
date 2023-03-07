977c2802b26374170e32d377fe1b9d74d6afd03d
hggks <carl.dahlen0068@stud.hkr.se>
2023-03-07 13:26:45 +0100

    test_highscore
    Skrev om de olika testerna

--- test_game/test_highscore.py
+++ test_game/test_highscore.py
@@ -1,32 +1,70 @@
-from pig_game.highscore import Highscore
 import unittest
+from pig_game.player import Player
+from pig_game.highscore import Highscore
+
 
 class TestHighscore(unittest.TestCase):
-    
-    def test_initial_scores(self):
-        """
-        Test if the initial scores for both the player and the bot are zero.
-        """
-        highscore = Highscore("Player1")
-        self.assertEqual(highscore.getplayer_score(), 0)
-        self.assertEqual(highscore.getbot_score(), 0)
+    def setUp(self):
+        self.hs = Highscore()
 
-    def test_set_scores(self):
-        """
-        Test if the player and bot scores are set correctly.
-        """
-        highscore = Highscore("Player2")
-        highscore.setPlayer_score(10)
-        highscore.setBot_score(5)
-        self.assertEqual(highscore.getplayer_score(), 10)
-        self.assertEqual(highscore.getbot_score(), 5)
+    def test_add_entry(self):
+        player1 = Player("Alice")
+        player2 = Player("Bob")
+        player3 = Player("Charlie")
 
-    def test_print_scoreboard(self):
-        """
-        Test if the scoreboard is printed correctly.
-        """
-        highscore = Highscore("Player3")
-        highscore.setPlayer_score(20)
-        highscore.setBot_score(15)
-        expected_output = "Scoreboard for Player3\nPlayer3: 20\nBot: 15\n"
-        self.assertEqual(highscore.print_scoreboard(), expected_output)
+        '''Add player1 to empty highscore'''
+        self.hs.add_entry(player1)
+        self.assertEqual(self.hs._entries, [player1])
+
+        '''Add player2 to non-empty highscore'''
+        self.hs.add_entry(player2)
+        self.assertEqual(self.hs._entries, [player1, player2])
+
+        '''Add player1 again with more wins'''
+        player1.set_nr_of_wins(3)
+        self.hs.add_entry(player1)
+        self.assertEqual(self.hs._entries, [player1, player2])
+
+        '''Add player3 with more wins than existing players'''
+        player3.set_nr_of_wins(5)
+        self.hs.add_entry(player3)
+        self.assertEqual(self.hs._entries, [player3, player1, player2])
+
+    def test_save_load_highscore(self):
+        player1 = Player("Alice")
+        player2 = Player("Bob")
+
+        self.hs.add_entry(player1)
+        self.hs.add_entry(player2)
+
+        ''' Save and load highscore '''
+        self.hs.save_highscore()
+        self.hs._entries = []
+        self.hs.load_highscore()
+        self.assertEqual(self.hs._entries, [player1, player2])
+
+    def test_get_highscore(self):
+        player1 = Player("Alice")
+        player2 = Player("Bob")
+
+        self.hs.add_entry(player1)
+        self.hs.add_entry(player2)
+
+        expected_output = "   Name    ----    Wins   \n"
+        expected_output += "  Alice    ----      1    \n"
+        expected_output += "   Bob     ----      1    \n"
+
+        self.assertEqual(self.hs.get_highscore(), expected_output)
+
+    def test_not_empty(self):
+        '''Empty highscore'''
+        self.assertFalse(self.hs.not_empty())
+
+        '''Non-empty highscore'''
+        player1 = Player("Alice")
+        self.hs.add_entry(player1)
+        self.assertTrue(self.hs.not_empty())
+
+
+if __name__ == '__main__':
+    unittest.main()
