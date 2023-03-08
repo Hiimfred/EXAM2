846852751611a2af2afe7cf15a1c93961e6ad99a
Lucas <lucas.lofqvist@gmail.com>
2023-03-08 15:06:20 +0100

    Update/changes in test_highscore.

--- test_highscore.py
+++ test_highscore.py
@@ -1,66 +1,36 @@
+"""Contains TestHighscore class."""
+
 import unittest
 import player
 import highscore
 
 
 class TestHighscore(unittest.TestCase):
+    """Methods that test the Highscore class."""
+
     def setUp(self):
+        """Set up a Highscore object to test."""
         self.hs = highscore.Highscore()
 
+    def test_highscore__init__(self):
+        """Test highscore initiation."""
+        self.assertFalse(self.hs._entries)
+
     def test_add_entry(self):
-        player1 = player.Player("Alice", "blue")
-        player2 = player.Player("Bob", "blue")
-        player3 = player.Player("Charlie", "blue")
+        """Test add_entry method."""
+        p1 = player.Player("Test", "red")
+        self.hs.add_entry(p1)
 
-        '''Add player1 to empty highscore'''
-        self.hs.add_entry(player1)
-        self.assertEqual(self.hs._entries, [player1])
+        length = len(self.hs._entries)
+        self.assertEqual(length, 1)
 
-        '''Add player2 to non-empty highscore'''
-        self.hs.add_entry(player2)
-        self.assertEqual(self.hs._entries, [player1, player2])
-
-        '''Add player1 again with more wins'''
-        player1.set_nr_of_wins(3)
-        self.hs.add_entry(player1)
-        self.assertEqual(self.hs._entries, [player1, player2])
-
-        '''Add player3 with more wins than existing players'''
-        player3.set_nr_of_wins(5)
-        self.hs.add_entry(player3)
-        self.assertEqual(self.hs._entries, [player3, player1, player2])
-
-    def test_save_load_highscore(self):
-        player1 = player.Player("Alice", "blue")
-        player2 = player.Player("Bob", "blue")
-
-        self.hs.add_entry(player1)
-        self.hs.add_entry(player2)
-
-        ''' Save and load highscore '''
-        self.hs.save_highscore()
-        self.hs._entries = []
-        self.hs.load_highscore()
-        self.assertEqual(self.hs._entries, [player1, player2])
-
-    def test_get_highscore(self):
-        player1 = player.Player("Alice", "blue")
-        player2 = player.Player("Bob", "blue")
-
-        self.hs.add_entry(player1)
-        self.hs.add_entry(player2)
-
-        expected_output = "   Name    ----    Wins   \n"
-        expected_output += "  Alice    ----      1    \n"
-        expected_output += "   Bob     ----      1    \n"
-
-        self.assertEqual(self.hs.get_highscore(), expected_output)
+        self.hs.add_entry(p1)
+        self.assertEqual(length, 1)
 
     def test_not_empty(self):
-        '''Empty highscore'''
+        """Test not_empty method."""
         self.assertFalse(self.hs.not_empty())
 
-        '''Non-empty highscore'''
         player1 = player.Player("Alice", "blue")
         self.hs.add_entry(player1)
         self.assertTrue(self.hs.not_empty())
