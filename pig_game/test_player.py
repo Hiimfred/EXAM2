266c42b7dca7f46d558ac148994d316b26ef3a8c
Hiimfred <Fredrik.ottenfelt0006@stud.hkr.se>
2023-03-09 15:47:12 +0100

    Update test_player.py

--- pig_game/test_player.py
+++ pig_game/test_player.py
@@ -16,40 +16,40 @@ class test_player(unittest.TestCase):
         Test module to get name, color and score
         to give correct value.
         """
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        self.assertEqual(p1.get_name(), "Fred")
-        self.assertEqual(p1.get_color(), Fore.BLUE)
-        self.assertEqual(p1.get_total_score(), 0)
-        self.assertEqual(p1.get_score(), 0)
+        self.assertEqual(test_player.get_name(), "Fred")
+        self.assertEqual(test_player.get_color(), Fore.BLUE)
+        self.assertEqual(test_player.get_total_score(), 0)
+        self.assertEqual(test_player.get_score(), 0)
 
     def test_get_name(self):
         """Check if get name is correct."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1._name = "Pelle"
-        self.assertEqual(p1.get_name(), "Pelle")
+        test_player._name = "Pelle"
+        self.assertEqual(test_player.get_name(), "Pelle")
 
     def test_set_name(self):
         """Check if set name is correct."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.set_name("123@")
-        self.assertEqual(p1._name, "123@")
+        test_player.set_name("123@")
+        self.assertEqual(test_player._name, "123@")
 
     def test_get_color(self):
         """Check if player get correct set color."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        self.assertEqual(p1.get_color(), Fore.BLUE)
+        self.assertEqual(test_player.get_color(), Fore.BLUE)
 
     def test_set_color(self):
         """Check if player set color works as intended."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.set_color("red")
-        self.assertEqual(p1.get_color(), Fore.RED)
-        self.assertRaises(ValueError, p1.set_color, "pink")
+        test_player.set_color("red")
+        self.assertEqual(test_player.get_color(), Fore.RED)
+        self.assertRaises(ValueError, test_player.set_color, "pink")
 
     def test_roll(self):
         """
@@ -60,48 +60,48 @@ class test_player(unittest.TestCase):
         over to next player else cotinue to roll and add points
         each roll until 1 or hold.
         """
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
         for i in range(10):
-            p1.set_score(0)
-            outcome = p1.roll(dice.Dice())
+            test_player.set_score(0)
+            outcome = test_player.roll(dice.Dice())
             self.assertTrue(outcome >= 1 and outcome <= 6)
             if outcome == 1:
-                self.assertEqual(p1.get_score(), 0)
+                self.assertEqual(test_player.get_score(), 0)
             else:
-                self.assertEqual(p1.get_score(), outcome)
+                self.assertEqual(test_player.get_score(), outcome)
 
     def test_hold(self):
         """Check if hold works and adds scoreto total score."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.set_total_score(0)
-        p1.set_score(50)
-        p1.hold()
-        self.assertEqual(p1.get_total_score(), 50)
-        self.assertEqual(p1.get_score(), 0)
+        test_player.set_total_score(0)
+        test_player.set_score(50)
+        test_player.hold()
+        self.assertEqual(test_player.get_total_score(), 50)
+        self.assertEqual(test_player.get_score(), 0)
 
     def test_add_win(self):
         """Check that wins add wins work properly."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.add_win()
-        self.assertEqual(p1.get_nr_of_wins(), 1)
-        p1.add_win()
-        self.assertEqual(p1.get_nr_of_wins(), 2)
+        test_player.add_win()
+        self.assertEqual(test_player.get_nr_of_wins(), 1)
+        test_player.add_win()
+        self.assertEqual(test_player.get_nr_of_wins(), 2)
 
     def test_get_nr_of_wins(self):
         """Check number of wins for player."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.set_nr_of_wins(5)
-        self.assertEqual(p1.get_nr_of_wins(), 5)
-        p1.set_nr_of_wins(10)
-        self.assertEqual(p1.get_nr_of_wins(), 10)
+        test_player.set_nr_of_wins(5)
+        self.assertEqual(test_player.get_nr_of_wins(), 5)
+        test_player.set_nr_of_wins(10)
+        self.assertEqual(test_player.get_nr_of_wins(), 10)
 
     def test_cheat(self):
         """Check if cheat works and give 99 points."""
-        p1 = player.Player("Fred", "blue")
+        test_player = player.Player("Fred", "blue")
 
-        p1.cheat()
-        self.assertEqual(p1.get_total_score(), 99)
+        test_player.cheat()
+        self.assertEqual(test_player.get_total_score(), 99)
